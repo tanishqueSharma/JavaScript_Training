8ad52aa76df94fdc92a5e5feda28998db954fe43
tanishqueSharma <142586530+tanishqueSharma@users.noreply.github.com>
2023-08-24 09:14:45 +0000

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = '33abs'
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -14,11 +14,25 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1=> true; 0=>false
 // ""=>false
 //"taani"=>true
 let someNum = 33
 let stringNum = String(someNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// Operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+let str1 = 'hello'
+let str2 = ' taani'
+let str3 = str1 + str2
+console.log(str3);
