0836195250771c7c938558087d59bac78e1b7dc6
tanishqueSharma <142586530+tanishqueSharma@users.noreply.github.com>
2023-08-24 09:27:05 +0000

    datatype

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,7 @@
+// Primitive - call by value
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Non-primitive/Reference type- call by reference
+// Arrays, Objects, Functions
+
+//JS is dynamically typed:datatype define karna is not zaruri
