6f7e58d59ed839cda9ffea28346c197cf4e366e9
tanishqueSharma <142586530+tanishqueSharma@users.noreply.github.com>
2023-08-24 07:25:07 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+// alert(3 + 3) // we are using node js not browser thats why error
+
+let name = "Taani"
+let age = 18
+let isLoggedIn = true
+// PRIMITIVE DATA TYPES
+// number 
+// bigInt
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+//
+// object
+
+console.log(typeof "Taani")
+
