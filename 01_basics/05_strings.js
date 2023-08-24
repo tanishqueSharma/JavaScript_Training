291c1644a803e5e77e10397457eacce884958c78
tanishqueSharma <142586530+tanishqueSharma@users.noreply.github.com>
2023-08-24 09:41:06 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = 'taani'
+const repoCount = 10
+// console.log(name + repoCount+' Value');
+// console.log(`Hello my name is ${name}`);
+
+const gameName = new String("Taani-sharma");
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+const newString = gameName.substring(0,4) // 4 not included
+// console.log(newString);
+const anotherString = gameName.slice(-1,4)
+// console.log(anotherString);
+
+const newStr1 = "     taani"
+
